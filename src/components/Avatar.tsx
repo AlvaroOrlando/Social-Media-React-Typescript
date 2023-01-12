9a561daff3aa1cabbd3acc7d0cb1a548225b6bfb
alvaro <alvarocondeorlando@gmail.com>
2023-01-12 19:25:00 -0300

    refatoração interface AvatarProps - implementando extenção de interface

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,10 +2,8 @@ import styles from './Avatar.module.css';
 import { ImgHTMLAttributes } from 'react';
 
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes <HTMLImageElement>{
   hasBorder?: boolean;
-  src:string;
-  alt?:string;
 }
 
 export function Avatar({ hasBorder = true, src, alt}:AvatarProps){
